8fc5dd7d00ad6a66b0a1f7f3721f4150ecde52ca
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2015-09-12 13:25:04 +0900

    frontend ES5

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -3,82 +3,80 @@
 
 'use strict';
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _jquery = require('jquery');
-
-var _jquery2 = _interopRequireDefault(_jquery);
-
 'use strct';
 
-var $roomInputForm = (0, _jquery2['default'])('#roomInputForm');
-var $roomInputButton = (0, _jquery2['default'])('#roomInputButton');
-var $roomName = (0, _jquery2['default'])('#roomName');
-var $messageList = (0, _jquery2['default'])('#messageList');
-var $messageInputForm = (0, _jquery2['default'])('#messageInputForm');
-var $messageInputButton = (0, _jquery2['default'])('#messageInputButton');
+var $ = require('jquery');
 
-var addMessages = function addMessages(messages) {
-  messages = Array.isArray(messages) ? messages : [messages];
+$(function () {
+  var $roomInputForm = $('#roomInputForm');
+  var $roomInputButton = $('#roomInputButton');
+  var $roomName = $('#roomName');
+  var $messageList = $('#messageList');
+  var $messageInputForm = $('#messageInputForm');
+  var $messageInputButton = $('#messageInputButton');
 
-  messages.forEach(function (message) {
-    $messageList.append('<li>' + message.content + '</li>');
+  var addMessages = function addMessages(messages) {
+    messages = Array.isArray(messages) ? messages : [messages];
+
+    messages.forEach(function (message) {
+      $messageList.append('<li>' + message.content + '</li>');
+    });
+  };
+
+  var currentRoom;
+
+  $roomInputButton.on('click', function () {
+    var name = $roomInputForm.val();
+
+    $.ajax({
+      method: 'POST',
+      url: '/room',
+      data: {
+        name: name
+      }
+    }).done(function (room) {
+      // TODO リストに追加
+      console.log(room);
+    }).fail(function (error) {
+      alert(error);
+      console.error(error);
+    });
   });
-};
 
-var currentRoom = undefined;
+  $('#roomList li').on('click', function (e) {
+    var name = $(this).text();
 
-$roomInputButton.on('click', function () {
-  var name = $roomInputForm.val();
+    $roomName.text('IN ' + name);
+    $messageList.empty();
 
-  _jquery2['default'].ajax({
-    method: 'POST',
-    url: '/room',
-    data: {
-      name: name
-    }
-  }).done(function (room) {
-    // TODO リストに追加
-    console.log(room);
-  }).fail(function (error) {
-    alert(error);
-    console.error(error);
+    $.ajax({
+      method: 'GET',
+      url: '/message?room=' + name
+    }).done(function (messages) {
+      currentRoom = name;
+      addMessages(messages);
+    }).fail(function (error) {
+      alert(error);
+      console.error(error);
+    });
   });
-});
 
-(0, _jquery2['default'])('#roomList li').on('click', function (e) {
-  var name = (0, _jquery2['default'])(e.target).text();
+  $messageInputButton.on('click', function () {
+    var content = $messageInputForm.val();
 
-  $roomName.text('IN ' + name);
-  $messageList.empty();
-
-  _jquery2['default'].ajax({
-    method: 'GET',
-    url: '/message?room=' + name
-  }).done(function (messages) {
-    currentRoom = name;
-    addMessages(messages);
-  }).fail(function (error) {
-    alert(error);
-    console.error(error);
-  });
-});
-
-$messageInputButton.on('click', function () {
-  var content = $messageInputForm.val();
-
-  _jquery2['default'].ajax({
-    method: 'POST',
-    url: '/message',
-    data: {
-      content: content,
-      room: currentRoom
-    }
-  }).done(function (message) {
-    addMessages(message);
-  }).fail(function (error) {
-    alert(error);
-    console.error(error);
+    $.ajax({
+      method: 'POST',
+      url: '/message',
+      data: {
+        content: content,
+        room: currentRoom
+      }
+    }).done(function (message) {
+      addMessages(message);
+    }).fail(function (error) {
+      alert(error);
+      console.error(error);
+    });
   });
 });
 
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,75 +2,77 @@
 
 'use strct';
 
-import $ from 'jquery';
+var $ = require('jquery');
 
-const $roomInputForm = $('#roomInputForm');
-const $roomInputButton = $('#roomInputButton');
-const $roomName = $('#roomName');
-const $messageList = $('#messageList');
-const $messageInputForm = $('#messageInputForm');
-const $messageInputButton = $('#messageInputButton');
+$(function() {
+  var $roomInputForm = $('#roomInputForm');
+  var $roomInputButton = $('#roomInputButton');
+  var $roomName = $('#roomName');
+  var $messageList = $('#messageList');
+  var $messageInputForm = $('#messageInputForm');
+  var $messageInputButton = $('#messageInputButton');
 
-const addMessages = function(messages) {
-  messages = Array.isArray(messages) ? messages : [messages];
+  var addMessages = function(messages) {
+    messages = Array.isArray(messages) ? messages : [messages];
 
-  messages.forEach((message) => {
-    $messageList.append(`<li>${message.content}</li>`);
+    messages.forEach(function(message) {
+      $messageList.append('<li>' + message.content + '</li>');
+    });
+  };
+
+  var currentRoom;
+
+  $roomInputButton.on('click', function() {
+    var name = $roomInputForm.val();
+
+    $.ajax({
+      method: 'POST',
+      url: '/room',
+      data: {
+        name: name
+      }
+    }).done(function(room) {
+      // TODO リストに追加
+      console.log(room);
+    }).fail(function(error) {
+      alert(error);
+      console.error(error);
+    });
   });
-};
 
-let currentRoom;
+  $('#roomList li').on('click', function(e) {
+    var name = $(this).text();
 
-$roomInputButton.on('click', () => {
-  const name = $roomInputForm.val();
+    $roomName.text('IN ' + name);
+    $messageList.empty();
 
-  $.ajax({
-    method: 'POST',
-    url: '/room',
-    data: {
-      name: name
-    }
-  }).done((room) => {
-    // TODO リストに追加
-    console.log(room);
-  }).fail((error) => {
-    alert(error);
-    console.error(error);
+    $.ajax({
+      method: 'GET',
+      url: `/message?room=${name}`
+    }).done(function(messages) {
+      currentRoom = name;
+      addMessages(messages);
+    }).fail(function(error){
+      alert(error);
+      console.error(error);
+    });
   });
-});
 
-$('#roomList li').on('click', (e) => {
-  var name = $(e.target).text();
+  $messageInputButton.on('click', function() {
+    var content = $messageInputForm.val();
 
-  $roomName.text(`IN ${name}`);
-  $messageList.empty();
-
-  $.ajax({
-    method: 'GET',
-    url: `/message?room=${name}`
-  }).done((messages) => {
-    currentRoom = name;
-    addMessages(messages);
-  }).fail((error) => {
-    alert(error);
-    console.error(error);
-  });
-});
-
-$messageInputButton.on('click', () => {
-  const content = $messageInputForm.val();
-
-  $.ajax({
-    method: 'POST',
-    url: '/message',
-    data: {
-      content: content,
-      room: currentRoom
-    }
-  }).done((message) => {
-    addMessages(message);
-  }).fail((error) => {
-    alert(error);
-    console.error(error);
+    $.ajax({
+      method: 'POST',
+      url: '/message',
+      data: {
+        content: content,
+        room: currentRoom
+      }
+    }).done(function(message) {
+      addMessages(message);
+    }).fail(function(error) {
+      alert(error);
+      console.error(error);
+    });
   });
 });
